81f045ec2a3c0746d01c09a7572ad36517d9c033
Critter & Guitari <critter@critterandguitari.com>
2017-08-18 18:53:09 +0000

    typos

--- run.sh
+++ run.sh
@@ -5,10 +5,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 oscsend localhost 4001 /oled/aux/clear i 1
 
-oscsend localhost 4001 /oled/aux/line/1 s "Starting Web Server..."
+oscsend localhost 4001 /oled/aux/line/1 s "Started Web Server."
 oscsend localhost 4001 /oled/aux/line/2 s "Vist here:"
-oscsend localhost 4001 /oled/aux/line/3 s "$IP"
-oscsend localhost 4001 /oled/aux/line/4 s "In your browser."
+oscsend localhost 4001 /oled/aux/line/3 s "$IP/files"
+oscsend localhost 4001 /oled/aux/line/4 s "with your browser."
 oscsend localhost 4001 /oled/aux/line/5 s " "
 
 # set to aux screen, signals screen update
